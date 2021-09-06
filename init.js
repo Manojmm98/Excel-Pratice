8a9767ec2182be631ccfdaba3f746db44d857e73
Manojmm98 <manojmishra98feb@gmail.com>
2021-09-07 00:55:25 +0530

    updated

--- init.js
+++ init.js
@@ -21,10 +21,30 @@ for(let i=0;i<=row;i++) {
 let row = document.createElement("div");
 row.setAttribute("class","row");
 for(let j=0;j<coloum;j++){
-    let div = document.createElement("div");
-    div.innerText = `${i} ${String.fromCharCode(65+j)}`;
-    div.setAttribute("class","cell");
-    row.appendChild(div);
+    let col = document.createElement("div");
+    col.setAttribute("class","cell");
+    //to give row and coloum unique id we used setattribute
+    col.setAttribute("rid",i);
+    col.setAttribute("cid",j);
+    col.setAttribute("contenteditable","true")
+    row.appendChild(col);
 }
 grid.appendChild(row);
 }
+//here we search allcells from grid
+let allcells = document.querySelectorAll(".grid .cell");
+let adresselements = document.querySelector(".adress");
+for(let i = 0; i < allcells.length; i++){
+    allcells[i].addEventListener("click",function(){
+        //find out rid and cid from all cells
+        let rid = allcells[i].getAttribute("rid");
+        let cid = allcells[i].getAttribute("cid");
+        // convert rid and cid into number
+        rid = Number(rid);
+        cid = Number(cid);
+        //put the rowid and cid in adress box we used 65 bcz 65==A 
+        adresselements.value = `${String.fromCharCode(65+cid)}${1+rid} `
+    })
+}
+
+
